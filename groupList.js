3c8685edf5e543d129035b3dc40894da85d32a1c
Xiuhong <miaozi1123@126.com>
2012-12-21 17:04:54 +0800

    A Group list

--- groupList.js
+++ groupList.js
@@ -1,31 +1,32 @@
 /* by miaoxiuhong 2012-12-17
 * 多级组
 */
-var mulitiplyList = function(o){
-	var f = {
+var mulitiplyList = function(o,u){
+	var p = {
 		data: function(){
 			var request = $.ajax({
-				url:'/jsonList.js',
+				url: u,
 				dataType: 'script',
 				cache: 'false',
 			});
-			request.done(function(data) {
-				var items = f.reorganization(eval(data).items),ulContent = [];
-				if (items.length > 0) {
-					$.each(items, function(i, item){
-						ulContent.push('<li groupId="' + item.id + '"><a class="l' + item.level + '" href="javascript:;">' + item.name + '</a></li>');
-					});
-					f.ui(ulContent.join(""));
-				}else{
-					var li = '<li class="none">您还没有企业通讯录</li>';
-					f.ui(li);
-				}
-			});
-			request.fail(function(jqXHR, textStatus) {
-				if(console && console.log){
-					 console.log( "Request failed: " + textStatus );
-				}	 
-			});	
+			request.done(p.requestDone);
+			request.fail(p.requestFail);
+		},
+		requestDone:function(data){
+			var items = p.reorganization(eval(data).items),ulContent = [];
+			if (items.length > 0) {
+				$.each(items, function(i, item){
+					ulContent.push('<li groupId="' + item.id + '"><a class="l' + item.level + '" href="javascript:;">' + item.name + '</a></li>');
+				});
+				
+				p.ui(ulContent.join(""));
+			}else{
+				var li = '<li class="none">您还没有企业通讯录</li>';
+				p.ui(li);
+			}
+		},
+		requestFail:function(jqXHR, textStatus){
+			throw( "Request failed: " + textStatus );
 		},
 		ui:function(content){
 			o.html('<ul>'+ content +'</ul>');
@@ -46,5 +47,36 @@ var mulitiplyList = function(o){
 			return itemsNew;
 		}
 	};
-	f.data();
+	var inherit = function(p){
+		if(p == null)throw TypeError();
+		if(Object.create){
+			return Object.create(p);
+		}
+		var t = typeof p;
+		if(t !== "object" && t !== "function"){
+			throw TypeError();
+		}
+		function f(){};
+		f.prototype = p;
+		return new f();
+	}
+	var m = inherit(p);
+	m.requestDone = function(data){
+		var items = p.reorganization(eval(data).items), ulContent = [];
+		if (items.length > 0) {
+			$.each(items, function(i, item){
+				ulContent.push('<li groupId="' + item.id + '"><div class="list_control">');
+				ulContent.push('<a class="list_btn_style icon_list_edit boxy_external" title="重命名" href="javascript:;"></a>');
+				ulContent.push('<a href="javascript:;" class="list_btn_style icon_list_del" title="删除"></a></div>');
+				ulContent.push('<span class="cut_groupName">' + item.name + '</span></li>');
+			});
+			p.ui(ulContent.join(""));
+		}
+		else {
+			var li = '<li class="none">您还没有企业通讯录</li>';
+			p.ui(li);
+		}
+	}
+	p.data();
+	m.data();
 };
